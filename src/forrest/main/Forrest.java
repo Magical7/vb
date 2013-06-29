4736bee4356387f70f2be93b264b7065dc5bd64b
Stijn van Winsen <stijnvanwinsen@gmail.com>
2013-06-29 17:20:21 +0200

    argument usage

--- src/forrest/main/Forrest.java
+++ src/forrest/main/Forrest.java
@@ -1,6 +1,7 @@
 package forrest.main;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintStream;
 
@@ -8,6 +9,8 @@ import org.antlr.runtime.ANTLRInputStream;
 import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.tree.CommonTree;
 import org.antlr.runtime.tree.CommonTreeNodeStream;
+
+import testfiles.ForrestTester;
 
 import TAM.Assembler;
 import TAM.Interpreter;
@@ -19,6 +22,8 @@ public class Forrest {
 	//Streams used for input and output
 	private InputStream in;
 	private PrintStream out;
+	
+	private ForrestOptions options;
 	
 	public Forrest(){
 		
@@ -36,10 +41,10 @@ public class Forrest {
 	 * @param runProgram - boolean: Whether to run the assembled program
 	 */
 	public void runForrest(String inputFile, PrintStream outputPS, 
-		boolean useParser, boolean useChecker, boolean useInterpreter, boolean useEncoder, boolean useTam, boolean runProgram, boolean showAST)
-		throws Exception{
+		ForrestOptions options) throws Exception{
+			this.options = options;
 			if(inputFile == null){
-				this.in = new FileInputStream(ForrestOptions.inputFileLocation);
+				this.in = new FileInputStream(options.inputFileLocation);
 			} else {
 				this.in = new FileInputStream(inputFile);
 			}
@@ -58,12 +63,12 @@ public class Forrest {
 			
 			ForrestParser.forrest_return result = parser.forrest();
 			CommonTree tree = (CommonTree) result.getTree();
-			if(useParser && showAST){
+			if(options.parser && options.showAst){
 				out.println(tree.toStringTree());
 			}
 			
 			//Check the Forrest program
-			if(useChecker){
+			if(options.checker){
 				CommonTreeNodeStream nodes = new CommonTreeNodeStream(tree);
 				ForrestChecker checker = new ForrestChecker(nodes);
 				checker.forrest();
@@ -77,21 +82,57 @@ public class Forrest {
 //			}
 			
 			//Generate code for the Forrest Program
-			if(useEncoder){
+			if(options.encoder){
 				CommonTreeNodeStream nodes = new CommonTreeNodeStream(tree);
 				ForrestEncoder encoder = new ForrestEncoder(nodes);
 				encoder.forrest();
 			}
 			
 			//Use the Tam assembler if needed
-			if(useTam){
-				Assembler.main(ForrestOptions.assemblerArguments);
+			if(options.useTam){
+				Assembler.main(options.assemblerArguments);
 			}
 			
 			//Run the program if needed
-			if(runProgram){
-				Interpreter.main(ForrestOptions.interpreterArguments);
+			if(options.runProgram){
+				Interpreter.main(options.interpreterArguments);
 			}
+	}
+	
+	private static boolean procesArgs(String[] args){
+		Forrest runner = new Forrest();
+		try{
+			if(args.length == 0){
+				return false;
+			} else if(args.length == 1) {
+				String arg = args[0];
+				if(arg.equals("-a") || arg.equals("-all")){
+					runner.runForrest(null, null, new ForrestOptions(true, true, false, true, true, true, false));
+				} else if(arg.equals("-t") || arg.equals("-tester")){
+					ForrestTester.main(null);
+				} else if(arg.equals("-d") || arg.equals("-default")){
+					runner.runForrest(null, null, new ForrestOptions());
+				} else {
+					return false;
+				}
+			} else if (args.length == 4) {
+				if(args[0].equals("-i") && args[2].equals("-o")){
+					runner.runForrest(null, null, new ForrestOptions(true, true, false, true, true, true, false, args[1], args[3], args[3]));
+				} else {
+					return false;
+				}
+			}
+		} catch (ForrestFireException e){
+			System.out.print("Fire!: ForrestException thrown by compiler ");
+			System.out.println(e.getMessage());
+		} catch (IOException e){
+			System.out.println(e.getMessage());
+		} catch (Exception e) {
+			System.out.print("ERROR: uncaught exception thrown by compiler: ");
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+	    }
+		return false;
 	}
 	
 	/**
@@ -99,16 +140,12 @@ public class Forrest {
 	 * @param args unused
 	 */
 	public static void main(String[] args){
-		Forrest runner = new Forrest();
-		try{
-			runner.runForrest(null, null, ForrestOptions.parser, ForrestOptions.checker, ForrestOptions.interpreter, ForrestOptions.encoder, ForrestOptions.useTam, ForrestOptions.runProgram, ForrestOptions.showParser);
-		} catch (ForrestFireException e){
-			System.out.print("Fire!: ForrestException thrown by compiler ");
-			System.out.println(e.getMessage());
-		} catch (Exception e) {
-			System.out.print("ERROR: uncaught exception thrown by compiler: ");
-			System.out.println(e.getMessage());
-			e.printStackTrace();
-	    }
+		if(!procesArgs(args)){
+			System.out.println("Arguments not valid. Please use the following:");
+			System.out.println("-a for all functions(Parser,checker,encoder,assembler and run the program");
+			System.out.println("-d for default usage (as defined in the file ForrestOptions.java)");
+			System.out.println("-i <inputFileName> -o <outputFileName> for all functions on specified file");
+			System.out.println("-t for running the Tester");
+		}
 	}
 }
--- src/forrest/main/ForrestOptions.java
+++ src/forrest/main/ForrestOptions.java
@@ -2,37 +2,93 @@ package forrest.main;
 
 public class ForrestOptions {
 	
+	/**
+	 * Constructor used for delivering the options to the Forrest program
+	 * @param parser - whether to use the parser
+	 * @param checker - whether to use the checker
+	 * @param interpreter - whether to use the interpreter - not available
+	 * @param encoder - whether to use the encoder
+	 * @param useTam - whether to use the TAM assembler
+	 * @param runProgram - whether to run the TAM program
+	 * @param inputFileLocation - location of the inputFile
+	 * @param tasmFileLocation - location of the tasmFile
+	 * @param tamFileLocation - location of the tamFile
+	 */
+	public ForrestOptions(boolean parser, boolean checker, 
+			boolean interpreter, boolean encoder, boolean useTam, 
+			boolean runProgram, boolean showAst,
+			String inputFileLocation, String tasmFileLocation, String tamFileLocation){
+		this.parser = parser;
+		this.checker = checker;
+		this.interpreter = interpreter;
+		this.encoder = encoder;
+		this.useTam = useTam;
+		this.runProgram = runProgram;
+		this.showAst = showAst;
+		this.inputFileLocation = "src/programs/" + inputFileLocation + ".forrest";
+		this.tasmFileLocation = "src/programs/" + tasmFileLocation + ".tasm";
+		this.tamFileLocation = "src/programs/" + tamFileLocation + ".tam";
+		this.assemblerArguments[0] = this.tasmFileLocation;
+		this.assemblerArguments[1] = this.tamFileLocation;
+		this.interpreterArguments[0] = this.tasmFileLocation;
+	}
+	
+	/**
+	 * Constructor used for delivering the options to the Forrest program
+	 * @param parser - whether to use the parser
+	 * @param checker - whether to use the checker
+	 * @param interpreter - whether to use the interpreter - not available
+	 * @param encoder - whether to use the encoder
+	 * @param useTam - whether to use the TAM assembler
+	 * @param runProgram - whether to run the TAM program
+	 */
+	public ForrestOptions(boolean parser, boolean checker, 
+			boolean interpreter, boolean encoder, boolean useTam, 
+			boolean runProgram, boolean showAst){
+		this.parser = parser;
+		this.checker = checker;
+		this.interpreter = interpreter;
+		this.encoder = encoder;
+		this.useTam = useTam;
+		this.runProgram = runProgram;
+		this.showAst = showAst;
+	}
+	
+	/**
+	 * Default constructor
+	 */
+	public ForrestOptions(){
+		
+	}
+	
 	//What should the program do?
 	/** Run the parser */
-	protected static final boolean parser = true;
+	protected boolean parser = true;
 	/** Run the checker */
-	protected static final boolean checker = true;
+	protected boolean checker = true;
 	/** Run the interpreter - unused */
-	protected static final boolean interpreter = false;
+	protected boolean interpreter = false;
 	/** Run the encoder */
-	protected static final boolean encoder = true;
+	protected boolean encoder = true;
 	/** Use TAM to convert the output to assembly */
-	protected static boolean useTam = true;
+	protected boolean useTam = true;
 	/** Use the TAM interpreter to run the program */
-	protected static boolean runProgram = true;
+	protected boolean runProgram = true;
 	
 	/** Show the output of the parser */
-	protected static final boolean showParser = false;
-	/** Show the output of the checker */
-	protected static final boolean showChecker = false;
-	
+	protected boolean showAst = false;
 	
 	/** Forrest file to read from */
-	protected static final String inputFileLocation = "src/testfiles/BlackBoxTest4.forrest";
+	protected String inputFileLocation = "src/testfiles/BlackBoxTest4.forrest";
 	/** TAM file to output to and read from */
-	protected static final String tasmFileLocation = "src/TAM/program.tasm";
+	protected String tasmFileLocation = "src/TAM/program.tasm";
 	/** TAM assembler file to output to and read from */
-	protected static final String tamFileLocation = "src/TAM/program.tam";
+	protected String tamFileLocation = "src/TAM/program.tam";
 	
 	/** Arguments to the TAM assembler */
-	protected static final String[] assemblerArguments = {tasmFileLocation, tamFileLocation};
+	protected String[] assemblerArguments = {tasmFileLocation, tamFileLocation};
 	/** Arguments tot the TAM interpreter */
-	protected static final String[] interpreterArguments = {tamFileLocation};
+	protected String[] interpreterArguments = {tamFileLocation};
 	
 	
 	
